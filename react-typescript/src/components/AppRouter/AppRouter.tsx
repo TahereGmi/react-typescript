a4f5a7a5c8bb9cb7932ed95d62ce5453cc3b9f03
Tahereh Gholami <mstgholami@gmail.com>
2021-12-19 01:03:11 +0330

    Add react-router-dom and create AppRouter

---
+++ react-typescript/src/components/AppRouter/AppRouter.tsx
@@ -0,0 +1,18 @@
+// import React from "react";
+// use direct import to include the page in the main bundle
+import Home from "pages/home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function AppRouter() {
+  return (
+    <Router>
+      <Switch>
+        <Route path={"/"}>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default AppRouter;
